92c5af711353ad49a56cc55fae7a577a664b14a4
Masa <masa25michi@hotmail.com>
2015-11-19 02:16:37 +0800

    Assignment 5 of Object Oriented Class by Student IDL 14116974D and Student NameL Masamichi Tanaka

---
+++ src/simpledatabase/Projection.java
@@ -0,0 +1,40 @@
+package simpledatabase;
+import java.util.ArrayList;
+
+public class Projection extends Operator{
+	
+	ArrayList<Attribute> newAttributeList;
+	private String attributePredicate;
+
+
+	public Projection(Operator child, String attributePredicate){
+		
+		this.attributePredicate = attributePredicate;
+		this.child = child;
+		newAttributeList = new ArrayList<Attribute>();
+		
+	}
+	
+	
+	/**
+     * Return the data of the selected attribute as tuple format
+     * @return tuple
+     */
+	@Override
+	public Tuple next(){
+		//Delete the lines below and add your code here
+		return null;
+		
+	}
+		
+
+	
+	/**
+     * The function is used to get the attribute list of the tuple
+     * @return attribute list
+     */
+	public ArrayList<Attribute> getAttributeList(){
+		return child.getAttributeList();
+	}
+
+}
---
+++ src/simpledatabase/Selection.java
@@ -0,0 +1,43 @@
+package simpledatabase;
+import java.util.ArrayList;
+
+public class Selection extends Operator{
+	
+	ArrayList<Attribute> attributeList;
+	String whereTablePredicate;
+	String whereAttributePredicate;
+	String whereValuePredicate;
+
+	
+	public Selection(Operator child, String whereTablePredicate, String whereAttributePredicate, String whereValuePredicate) {
+		this.child = child;
+		this.whereTablePredicate = whereTablePredicate;
+		this.whereAttributePredicate = whereAttributePredicate;
+		this.whereValuePredicate = whereValuePredicate;
+		attributeList = new ArrayList<Attribute>();
+
+	}
+	
+	
+	/**
+     * Get the tuple which match to the where condition
+     * @return the tuple
+     */
+	@Override
+	public Tuple next(){
+		//Delete the lines below and add your code here
+		return null;
+			
+	}
+	
+	/**
+     * The function is used to get the attribute list of the tuple
+     * @return the attribute list
+     */
+	public ArrayList<Attribute> getAttributeList(){
+		
+		return(child.getAttributeList());
+	}
+
+	
+}
